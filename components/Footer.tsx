ee7b3d42c7f5dadb14a66b49016b88432991611b
Ethan Orain <ethan.orain91@gmail.com>
2025-08-15 12:23:07 +0200

    Normalize line endings to LF

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-// components/Footer.tsx
-export default function Footer() {
-    return (
-      <footer className="text-center p-4 bg-primary text-sm text-muted-primary-fg">
-        © {new Date().getFullYear()} Ethan Orain. All rights reserved.
-      </footer>
-    );
-  }
-  
